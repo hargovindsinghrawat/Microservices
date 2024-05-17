143e6840c6198f177ed98f8d5f9aa39e7ab1461f
hargovindsinghrawat <hargovindsinghrwt@gmail.com>
2024-05-17 10:51:45 +0530

    Microservices

---
+++ currency-exchange-service/currency-exchange-service/src/test/java/com/hargovind/microservice/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hargovind.microservice.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
